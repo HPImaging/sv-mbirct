0d09bc39fac93dff92f33b94913a9f57d578e64c
Sherman J. Kisner <sjkisner@gmail.com>
2019-10-25 01:56:55 -0400

    Removed Gen_SysMatrix.c and some un-needed functions

--- src/initialize.c
+++ src/initialize.c
@@ -145,51 +145,5 @@ void NormalizePriorWeights3D(struct ReconParamsQGGMRF3D *reconparams)
     reconparams->b_interslice /= sum;
 }
 
-/* Wrapper to read in Image, sinogram and reconstruction parameters */
-void readSystemParams(
-	struct CmdLine *cmdline,
-	struct ImageParams3D *imgparams,
-	struct SinoParams3DParallel *sinoparams,
-	struct ReconParamsQGGMRF3D *reconparams)
-{
-    //printf("\nReading Image, Sinogram and Reconstruction Parameters ... \n");
-    
-    if(ReadImageParams3D(cmdline->ImageParamsFile, imgparams)) {
-        fprintf(stdout,"Error in reading image parameters \n");
-        exit(-1);
-    }
-    if(ReadSinoParams3DParallel(cmdline->SinoParamsFile, sinoparams)) {
-        fprintf(stdout,"Error in reading sinogram parameters \n");
-        exit(-1);
-    }
-    if(ReadReconParamsQGGMRF3D(cmdline->ReconParamsFile ,reconparams)) {
-        fprintf(stdout,"Error in reading reconstruction parameters \n");
-        exit(-1);
-    }
-          
-    /* Tentatively initialize weights. Remove once this is read in directly from params file */
-    NormalizePriorWeights3D(reconparams);
-    
-    /* Print paramters */
-    //printSinoParams3DParallel(sinoparams);
-    //printImageParams3D(imgparams);
-    //printReconParamsQGGMRF3D(reconparams);
-    //fprintf(stdout,"\n");
-
-    /* Determine and SET number of slice index digits in data files */
-    int Ndigits = NumSinoSliceDigits(cmdline->SinoDataFile, sinoparams->FirstSliceNumber);
-    if(Ndigits==0)
-    {
-        fprintf(stderr,"Error: Can't read the first data file. Looking for any one of the following:\n");
-        for(int i=MBIR_MODULAR_MAX_NUMBER_OF_SLICE_DIGITS; i>0; i--)
-            fprintf(stderr,"\t%s_slice%.*d.2Dsinodata\n",cmdline->SinoDataFile, i, sinoparams->FirstSliceNumber);
-        exit(-1);
-    }
-    //printf("Detected %d slice index digits\n",Ndigits);
-    sinoparams->NumSliceDigits = Ndigits;
-    imgparams->NumSliceDigits = Ndigits;
-
-}
-
 
 
