1feb39450a144f5e5fc62601ce561c0495d6f385
Sherman J. Kisner <sjkisner@gmail.com>
2020-12-08 15:12:15 -0500

    Update allocate.[ch]

--- src/allocate.c
+++ src/allocate.c
@@ -1,19 +1,17 @@
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <stdarg.h>
-
 #include "allocate.h"
 
 void *get_spc(size_t num, size_t size)
 {
 	void *pt;
 
-	if ((pt = calloc(num,size)) == NULL)
-	{
-		fprintf(stderr, "get_spc: calloc() error\n");
+	if( (pt=calloc(num,size)) == NULL ) {
+		fprintf(stderr, "get_spc(): calloc() error\n");
 		exit(-1);
-	}
+		}
 	return(pt);
 }
 
@@ -21,54 +19,40 @@ void *mget_spc(size_t num, size_t size)
 {
 	void *pt;
 
-	if ((pt = malloc(num*size)) == NULL)
-	{
-		fprintf(stderr, "mget_spc: malloc() error\n");
+	if( (pt=malloc(num*size)) == NULL ) {
+		fprintf(stderr, "mget_spc(): malloc() error\n");
 		exit(-1);
-	}
+		}
 	return(pt);
 }
 
-
-void **get_img(int wd,int ht,size_t size)
+void **get_img(size_t wd,size_t ht,size_t size)
 {
-	void *pt;
+	size_t i;
+	void  **ppt;
+	char   *pt;
 
-	if ((pt = multialloc(size,2,ht,wd)) == NULL)
-	{
-		fprintf(stderr, "get_img: out of memory\n");
-		exit(-1);
-	}
-	return((void **)pt);
+	ppt = (void **)mget_spc(ht,sizeof(void *));
+	pt = (char *)mget_spc(wd*ht,size);
+
+	for(i=0; i<ht; i++) ppt[i] = pt + i*wd*size;
+
+	return(ppt);
 }
-
-void ***get_3D(int N, int M, int A, size_t size)
-{
-	void *pt;
-
-	if ((pt = multialloc(size,3,N,M,A)) == NULL)
-	{
-		fprintf(stderr, "get_3D: out of memory\n");
-		exit(-1);
-	}
-	return((void ***)pt);
-}
-
 
 void free_img(void **pt)
 {
-	multifree((void *)pt, 2);
+	free( (void *)pt[0]);
+	free( (void *)pt);
 }
 
-void free_3D(void ***pt)
-{
-	multifree((void *)pt, 3);
-}
 
 
 
 /* modified from dynamem.c on 4/29/91 C. Bouman                           */
 /* Converted to ANSI on 7/13/93 C. Bouman         	                  */
+/* Modified for 1-D case on 6/29/95 C. Bouman         	                  */
+/* Modified for 64 bit memory allocation 8/29/2013 A. Mohan               */
 /* multialloc( s, d,  d1, d2 ....) allocates a d dimensional array, whose */
 /* dimensions are stored in a list starting at d1. Each array element is  */
 /* of size s.                                                             */
@@ -76,73 +60,82 @@ void free_3D(void ***pt)
 
 void *multialloc(size_t s, int d, ...)
 {
-	va_list ap;             /* varargs list traverser */
-	size_t max;             /* size of array to be declared */
-	int *q;                 /* pointer to dimension list */
-	char **r,               /* pointer to beginning of the array of the
-				 * pointers for a dimension */
-	     **s1, *t, *tree;        /* base pointer to beginning of first array */
-	int i, j;               /* loop counters */
-	int *d1;                /* dimension list */
+        va_list ap;             /* varargs list traverser */
+        size_t max;             /* size of array to be declared */
+        size_t j;               /* loop counter */
+        size_t *d1,*q;          /* pointer to dimension list */
+        char **r;               /* pointer to beginning of the array of the
+                                 * pointers for a dimension */
+        char **s1, *t, *tree;   /* base pointer to beginning of first array */
+        int i;                  /* loop counter */
 
-	va_start(ap,d);
-	d1 = (int *) mget_spc((size_t)d,sizeof(int));
+        va_start(ap,d);
+        d1 = (size_t *) mget_spc(d,sizeof(size_t));
 
-	for(i=0;i<d;i++)
-		d1[i] = va_arg(ap,int);
+        for(i=0;i<d;i++)
+          d1[i] = va_arg(ap,size_t);
 
-	r = &tree;
-	q = d1;                /* first dimension */
-	max = 1;
-	for (i = 0; i < d - 1; i++, q++) {      /* for each of the dimensions
-						 * but the last */
-		max *= (*q);
-		r[0]=(char *)mget_spc(max,sizeof(char **));
-		r = (char **) r[0];     /* step through to beginning of next
-					 * dimension array */
-	}
-	max *= s * (*q);        /* grab actual array memory */
-	r[0] = (char *)mget_spc(max,sizeof(char));
 
-	/*
-	 * r is now set to point to the beginning of each array so that we can
-	 * use it to scan down each array rather than having to go across and
-	 * then down 
-	 */
-	r = (char **) tree;     /* back to the beginning of list of arrays */
-	q = d1;                 /* back to the first dimension */
-	max = 1;
-	for (i = 0; i < d - 2; i++, q++) {      /* we deal with the last
-						 * array of pointers later on */
-		max *= (*q);    /* number of elements in this dimension */
-		for (j=1, s1=r+1, t=r[0]; j<max; j++) { /* scans down array for
-							 * first and subsequent
-							 * elements */
+        /* Take care of 1-D case separately (6/29/95) */
+        if( d==1 ) {
+          tree = (char *)mget_spc(d1[0],s*sizeof(char));
+          free((void *)d1);
+          return((void *)tree);              /* return base pointer */
+        }
 
-			/*  modify each of the pointers so that it points to
-			 * the correct position (sub-array) of the next
-			 * dimension array. s1 is the current position in the
-			 * current array. t is the current position in the
-			 * next array. t is incremented before s1 is, but it
-			 * starts off one behind. *(q+1) is the dimension of
-			 * the next array. */
 
-			*s1 = (t += sizeof (char **) * *(q + 1));
-			s1++;
-		}
-		r = (char **) r[0];     /* step through to begining of next
-					 * dimension array */
-	}
-	max *= (*q);              /* max is total number of elements in the
-				   * last pointer array */
+        r = &tree;
+        q = d1;                /* first dimension */
+        max = 1;
+        for (i = 0; i < d - 1; i++, q++) {      /* for each of the dimensions
+                                                 * but the last */
+          max *= (*q);
+          r[0]=(char *)mget_spc(max,sizeof(char **));
+          r = (char **) r[0];     /* step through to beginning of next
+                                   * dimension array */
+        }
+        max *= s * (*q);        /* grab actual array memory */
+        r[0] = (char *)mget_spc(max,sizeof(char));
 
-	/* same as previous loop, but different size factor */
-	for (j = 1, s1 = r + 1, t = r[0]; j < max; j++) 
-		*s1++ = (t += s * *(q + 1));
+        /*
+         * r is now set to posize_t to the beginning of each array so that we can
+         * use it to scan down each array rather than having to go across and
+         * then down 
+         */
+        r = (char **) tree;     /* back to the beginning of list of arrays */
+        q = d1;                 /* back to the first dimension */
+        max = 1;
+        for (i = 0; i < d - 2; i++, q++) {      /* we deal with the last
+                                                 * array of pointers later on */
+          max *= (*q);    /* number of elements in this dimension */
+          for (j=1, s1=r+1, t=r[0]; j<max; j++) { /* scans down array for
+                                                   * first and subsequent
+                                                   * elements */
 
-	va_end(ap);
-	free((void *)d1);
-	return((void *)tree);              /* return base pointer */
+          /*  modify each of the pointers so that it points to
+           * the correct position (sub-array) of the next
+           * dimension array. s1 is the current position in the
+           * current array. t is the current position in the
+           * next array. t is incremented before s1 is, but it
+           * starts off one behind. *(q+1) is the dimension of
+           * the next array. */
+
+            *s1 = (t += sizeof (char **) * *(q + 1));
+            s1++;
+          }
+          r = (char **) r[0];     /* step through to begining of next
+                                   * dimension array */
+        }
+        max *= (*q);              /* max is total number of elements in the
+                                   * last pointer array */
+
+        /* same as previous loop, but different size factor */
+        for (j = 1, s1 = r + 1, t = r[0]; j < max; j++)
+          *s1++ = (t += s * *(q + 1));
+
+        va_end(ap);
+        free((void *)d1);
+        return((void *)tree);              /* return base pointer */
 }
 
 
@@ -151,15 +144,17 @@ void *multialloc(size_t s, int d, ...)
  * multifree releases all memory that we have already declared analogous to
  * free() when using malloc() 
  */
-void multifree(void *r,int d)
+void multifree(void *r, int d)
 {
-	void **p;
-	void *next=NULL;
-	int i;
+        void **p;
+        void *next=NULL;
+        int i;
 
-	for (p = (void **)r, i = 0; i < d; p = (void **) next,i++)
-		if (p != NULL) {
-			next = *p;
-			free((void *)p);
-		}
+        for (p = (void **)r, i = 0; i < d; p = (void **) next,i++)
+          if (p != NULL) {
+            next = *p;
+            free((void *)p);
+            }
 }
+
+
--- src/allocate.h
+++ src/allocate.h
@@ -1,17 +1,15 @@
-
-#ifndef ALLOCATE_INC
-#define ALLOCATE_INC
+#ifndef _ALLOCATE_H_
+#define _ALLOCATE_H_
 
 #include <stdlib.h>
 
 void *get_spc(size_t num, size_t size);
 void *mget_spc(size_t num, size_t size);
-void **get_img(int wd,int ht, size_t size);
-void ***get_3D(int N, int M, int A, size_t size);
+void **get_img(size_t wd, size_t ht, size_t size);
 void free_img(void **pt);
-void free_3D(void ***pt);
 void *multialloc(size_t s, int d, ...);
-void multifree(void *r,int d);
+void multifree(void *r, int d);
+
+#endif /* _ALLOCATE_H_ */
 
 
-#endif
